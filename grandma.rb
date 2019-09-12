480369cca3ff00b40fa9b085f6a4eb3e5e976c7d
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-12 08:57:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase.upcase
+    return "HUH?!SPEAK UP, SONNY!"
   end
 end
   
